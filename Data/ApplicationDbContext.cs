c3bfb51a8913eedd502e1505a0cd58392ecbe044
David Tran <109535790+david125tran@users.noreply.github.com>
2023-07-22 15:35:21 -0400

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Library.Model;
+
+namespace Library.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Library.Model.Book>? Book { get; set; }
+    }
+}
