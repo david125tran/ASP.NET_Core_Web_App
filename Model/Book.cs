fd88c8dfe7b4bb256697b167cbd29216064be468
David Tran <109535790+david125tran@users.noreply.github.com>
2023-07-22 15:35:29 -0400

    Add files via upload

---
+++ Model/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Model
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
