e2afa443dbf1bed9f4088733d9b7ba33dc98e3ff
David Tran <109535790+david125tran@users.noreply.github.com>
2023-07-22 15:36:43 -0400

    Add files via upload

---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Library.Data;
+using Library.Model;
+
+namespace Library.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Library.Data.ApplicationDbContext _context;
+
+        public IndexModel(Library.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book.ToListAsync();
+            }
+        }
+    }
+}
